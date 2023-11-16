f79e1fef4a93375d01ae6e24bfd6540b8173e495
luc <luc.leray@gmail.com>
2023-11-16 16:05:24 +0100

    add stress-ng.sh

---
+++ stress-ng.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+if grep -q "Amazon Linux" /etc/os-release
+then
+  amazon-linux-extras install epel -y
+  yum install stress-ng -y
+else
+  add-apt-repository ppa:colin-king/stress-ng
+  apt update
+  apt install stress-ng
+fi
+
+stress-ng --cpu 4 --iomix 2 --vm 1 --vm-bytes 1G --timeout 60s --metrics-brief
